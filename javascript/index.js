4ab6a987c6b7ca16ff312bf4fc23edd50e0a8f29
tayba7777 <tayba.elias.moh@gmail.com>
2025-01-28 15:11:55 +0200

    added link for home page

--- javascript/index.js
+++ javascript/index.js
@@ -40,6 +40,7 @@ function updateCity(event) {
             </div>
             <div class="time">${cityTime.format("hh:mm:ss")}<small>${cityTime.format("A")}</small></div>
           </div>
+          <a href="index.html">All cities</a>
     `;
 }
 updateTime();
