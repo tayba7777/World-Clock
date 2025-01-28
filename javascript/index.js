4ec95be0f3f86a1a5e68367fc56b1c7f703f0ddb
tayba7777 <tayba.elias.moh@gmail.com>
2025-01-28 14:53:05 +0200

    added current locatin

--- javascript/index.js
+++ javascript/index.js
@@ -26,6 +26,9 @@ parisTimeElement.innerHTML = parisTime.format("hh:mm:ss[<small>]A[</small>]");
 
 function updateCity(event) {
     let cityTimeZone = event.target.value;
+    if (cityTimeZone === `current`) {
+      cityTimeZone = moment.tz.guess();
+    }
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
